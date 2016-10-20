e8bfb66aa2c643fe8357fe2a2c70617727fca7d6
xur <645531705@qq.com>
2016-10-20 15:11:11 +0800

    201610201511

--- app/src/main/java/com/example/xur/databasetest/MainActivity.java
+++ app/src/main/java/com/example/xur/databasetest/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.xur.databasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private MyDataBaseHelper dataBaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataBaseHelper=new MyDataBaseHelper(this,"BookStore.db",null,1);
+        Button createDatabase=(Button)findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataBaseHelper.getWritableDatabase();
+            }
+        });
     }
 }
