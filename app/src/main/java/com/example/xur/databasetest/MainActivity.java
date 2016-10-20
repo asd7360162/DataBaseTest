e1390da4fa966a6e35f3083c61daa4c70875a973
xur <645531705@qq.com>
2016-10-20 17:16:37 +0800

    201610201716

--- app/src/main/java/com/example/xur/databasetest/MainActivity.java
+++ app/src/main/java/com/example/xur/databasetest/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
