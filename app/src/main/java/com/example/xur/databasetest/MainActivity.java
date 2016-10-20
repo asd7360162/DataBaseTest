175945f2067cc6b230bd10826254b81d7ca794ef
xur <645531705@qq.com>
2016-10-20 16:36:33 +0800

    201610201636

--- app/src/main/java/com/example/xur/databasetest/MainActivity.java
+++ app/src/main/java/com/example/xur/databasetest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.xur.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private MyDataBaseHelper dataBaseHelper;
@@ -17,6 +20,28 @@ private MyDataBaseHelper dataBaseHelper;
             @Override
             public void onClick(View v) {
                 dataBaseHelper.getWritableDatabase();
+            }
+        });
+        Button addData=(Button)findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db=dataBaseHelper.getWritableDatabase();
+                ContentValues values=new ContentValues();
+                //开始组装第一条数据
+                values.put("name","The Da Vinci Code");
+                values.put("author","Dan Brown");
+                values.put("pages",454);
+                values.put("price",16.96);
+                db.insert("Book",null,values);//插入第一条数据
+                values.clear();
+                //开始组装第一条数据
+                values.put("name","The Lost Symble");
+                values.put("author","Dan Brown");
+                values.put("pages",444);
+                values.put("price",10.96);
+                db.insert("Book",null,values);//插入第二条数据
+                Toast.makeText(MainActivity.this,"插入成功",Toast.LENGTH_SHORT).show();
             }
         });
     }
