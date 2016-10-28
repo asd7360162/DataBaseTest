f67db22b798f0b313de668d0d80b280faa988bed
xur <645531705@qq.com>
2016-10-28 10:56:14 +0800

    201610281056

--- app/src/main/java/com/example/xur/databasetest/MainActivity.java
+++ app/src/main/java/com/example/xur/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.xur.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -65,6 +67,29 @@ private MyDataBaseHelper dataBaseHelper;
                 SQLiteDatabase db=dataBaseHelper.getWritableDatabase();
                 db.delete("Book","pages>?",new String[] { "500" });
                 Toast.makeText(MainActivity.this,"删除成功",Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button queryData=(Button)findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db=dataBaseHelper.getWritableDatabase();
+                //查询Book表中所有数据
+                Cursor cursor=db.query("Book",null,null,null,null,null,null);
+                if(cursor.moveToFirst()){
+                    do{
+                        //遍历Cursor对象，取出数据并打印
+                        String name=cursor.getString(cursor.getColumnIndex("name"));
+                        String author=cursor.getString(cursor.getColumnIndex("author"));
+                        int pages=cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price=cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity","book name is "+name);
+                        Log.d("MainActivity","book author is "+author);
+                        Log.d("MainActivity","book pages is "+pages);
+                        Log.d("MainActivity","book price is "+price);
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
